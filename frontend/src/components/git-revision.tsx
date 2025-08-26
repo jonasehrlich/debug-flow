c9b01f1c54868dda1a5836a062992e8f9aa15cd9
Tobias Waurick <tobtherock@proton.me>
2025-08-26 21:39:55 +0200

    feat(frontend): highlight pinned nodes per mouseover

--- frontend/src/components/git-revision.tsx
+++ frontend/src/components/git-revision.tsx
@@ -13,6 +13,8 @@ const selector = (s: AppState) => ({
   checkoutGitRevision: s.checkoutGitRevision,
   clearPinnedNodes: s.clearPinnedNodes,
   pinnedNodes: s.pinnedNodes,
+  highlightPinnedNode: s.highlightPinnedNode,
+  clearHighlightedNode: s.clearHighlightedNode,
 });
 
 interface GitRevisionIconProps {
@@ -40,8 +42,14 @@ interface GitRevisionProps {
   nodeId: string;
 }
 export const GitRevision = ({ revision, nodeId }: GitRevisionProps) => {
-  const { addPinnedNode, checkoutGitRevision, clearPinnedNodes, pinnedNodes } =
-    useStore(useShallow(selector));
+  const {
+    addPinnedNode,
+    checkoutGitRevision,
+    clearPinnedNodes,
+    pinnedNodes,
+    highlightPinnedNode,
+    clearHighlightedNode,
+  } = useStore(useShallow(selector));
   const [pinnedState, setPinnedState] = React.useState<PinnedState>(
     PinnedState.NotPinned,
   );
